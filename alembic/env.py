d6ab2e7cbd8caffdb653ceb96978299233b23d27
Patrycja Szpakowska <patrycja.szpakowska@gmail.com>
2026-05-12 13:11:26 +0200

    Register new models

--- alembic/env.py
+++ alembic/env.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 import app.client.models  # noqa: F401
 import app.exercise.models  # noqa: F401
+import app.log.models  # noqa: F401
 import app.plan.models  # noqa: F401
 import app.recipe.models  # noqa: F401
 import app.session.models  # noqa: F401
